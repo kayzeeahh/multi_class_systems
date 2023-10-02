5ff5c9c9357994dfeb232ca6792ad71d7b06c78c
Keziah <keziah.qrsh@gmail.com>
2023-10-02 09:10:17 +0100

    testing for diary and diary entry complete

--- a.py
+++ a.py
@@ -12,11 +12,15 @@ class Diary():
         'We want to take the title and content from withing the entry'
         'We want to format it in the form of Title: Content'
         
-        return '\n\n'.join([entry.format_entry() for entry in self.diary])
+        return '/n/n'.join([entry.format_entry() for entry in self.diary])#self.diary 
         
     def reading_now(self,wpm,minutes):
         words = wpm * minutes
-        return '\n\n'.join([entry.format_entry() for entry in self.diary if words >= entry.content_length()])
+        readable_entry = []
+        for diary_entry in self.diary:
+            if words >= diary_entry.content_length():
+                readable_entry.append(diary_entry)
+        return readable_entry 
 
 class DiaryEntry():
     def __init__(self, title, content):
@@ -28,3 +32,13 @@ class DiaryEntry():
     
     def format_entry(self):
         return f'{self.title}: {self.content}'
+    
+diary_ent1 = DiaryEntry('My title', 'This is my title my really lovely title')
+diary_ent2 = DiaryEntry('Yet another title.', 'This one is for you.') 
+
+secret_diary = Diary()
+
+Diary.add(diary_ent1)
+Diary.add(diary_ent2)
+
+print(secret_diary.read())
--- designing-class-systems/tests/test_diary.py
+++ designing-class-systems/tests/test_diary.py
@@ -2,7 +2,7 @@ from lib.diary import DiaryEntry
 from lib.diary import Diary
 from unittest.mock import *
 
-def test_add_to_diary():
+def test_add_to_diary_using_mock():
     diary = Diary()
     entry = Mock()
     
@@ -13,6 +13,13 @@ def test_add_to_diary():
 
 
 
+def test_for_reading_now():
+    diary = Diary()
+    diary_ent1 = DiaryEntry('My title', '1 2 3 4 5 6 7 8 9')
+    diary_ent2 = DiaryEntry('Yet another title', '1 2 3 4') 
+    diary.add(diary_ent1)
+    diary.add(diary_ent2)
+    assert diary.reading_now(4,2) == diary_ent2.format_entry()
 
 
 
